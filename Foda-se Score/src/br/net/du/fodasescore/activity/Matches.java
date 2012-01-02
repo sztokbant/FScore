8bab06f60158026b6b2f1771a33357e2fa988709
Eduardo Sztokbant <du@du.net.br>
2012-01-02 13:13:29 -0200

    project skeleton

---
+++ Foda-se Score/src/br/net/du/fodasescore/activity/Matches.java
@@ -0,0 +1,15 @@
+package br.net.du.fodasescore.activity;
+
+import br.net.du.fodasescore.R;
+import br.net.du.fodasescore.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Matches extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ Foda-se Score/src/br/net/du/fodasescore/model/Match.java
@@ -0,0 +1,5 @@
+package br.net.du.fodasescore.model;
+
+public class Match {
+
+}
