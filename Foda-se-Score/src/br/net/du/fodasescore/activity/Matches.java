98219a90a451741e4e92c463a7c95bd27e1a756f
Eduardo Sztokbant <du@du.net.br>
2012-01-07 15:56:50 -0200

    fixed layout name

--- Foda-se-Score/src/br/net/du/fodasescore/activity/Matches.java
+++ Foda-se-Score/src/br/net/du/fodasescore/activity/Matches.java
@@ -1,15 +1,14 @@
 package br.net.du.fodasescore.activity;
 
-import br.net.du.fodasescore.R;
-import br.net.du.fodasescore.R.layout;
 import android.app.Activity;
 import android.os.Bundle;
+import br.net.du.fodasescore.R;
 
 public class Matches extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.matches);
     }
 }
