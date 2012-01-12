1bda0bb8d59273cfb7b0eecfb3a9475d59c52d2b
Eduardo Sztokbant <du@du.net.br>
2012-01-12 13:44:16 -0200

    beginning implementation of choosing players from phone's contacts (based on Android in Action, 3rd ed, 15.3.1)

--- Foda-se-Score/src/br/net/du/fodasescore/activity/RoundList.java
+++ Foda-se-Score/src/br/net/du/fodasescore/activity/RoundList.java
@@ -3,15 +3,25 @@ package br.net.du.fodasescore.activity;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import br.net.du.fodasescore.R;
 import br.net.du.fodasescore.model.Match;
 import br.net.du.fodasescore.model.Round;
 
 public class RoundList extends Activity {
 	private Match match;
+
+	public static final int CONTACT_SELECTED_RESULT_ID = 1;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,5 +36,49 @@ public class RoundList extends Activity {
 		roundList.setAdapter(adapter);
 
 		roundList.setClickable(true);
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuItem addPlayer = menu.add(0, 0, 0, "Add Player");
+
+		final Intent chooser = new Intent(Intent.ACTION_PICK,
+				ContactsContract.Contacts.CONTENT_URI);
+		addPlayer.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				startActivityForResult(chooser, CONTACT_SELECTED_RESULT_ID);
+				return false;
+			}
+		});
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == 0) {
+			Toast.makeText(RoundList.this, "'Add Player'", Toast.LENGTH_SHORT)
+					.show();
+		}
+		return false;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		switch (requestCode) {
+		case (CONTACT_SELECTED_RESULT_ID):
+			if (resultCode == Activity.RESULT_OK) {
+				Uri contactData = data.getData();
+				Cursor c = managedQuery(contactData, null, null, null, null);
+				if (c.moveToFirst()) {
+					int nameIndex = c
+							.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME);
+					String name = c.getString(nameIndex);
+					Toast.makeText(this, name, 2000).show();
+				}
+			}
+		}
 	}
 }
