f56864658e51d7c125b325613d74cc0e227d1c80
Eduardo Sztokbant <du@du.net.br>
2012-01-21 20:52:57 -0200

    update method made dummy

--- FScore/src/br/net/du/fscore/persist/PlayerDAO.java
+++ FScore/src/br/net/du/fscore/persist/PlayerDAO.java
@@ -38,8 +38,8 @@ public class PlayerDAO implements Dao<Player> {
 
 	@Override
 	public void update(Player player) {
-		db.update(PlayerTable.NAME, toContentValues(player), BaseColumns._ID
-				+ " = ?", new String[] { String.valueOf(player.getId()) });
+//		db.update(PlayerTable.NAME, toContentValues(player), BaseColumns._ID
+//				+ " = ?", new String[] { String.valueOf(player.getId()) });
 	}
 
 	public void delete(Player player) {
