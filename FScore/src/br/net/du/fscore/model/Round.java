ef22150e9d21512813547bb82e60540dc867b4e1
Eduardo Sztokbant <du@du.net.br>
2012-01-22 20:18:19 -0200

    Round class implementation getting ready for persistence

--- FScore/src/br/net/du/fscore/model/Round.java
+++ FScore/src/br/net/du/fscore/model/Round.java
@@ -3,17 +3,78 @@ package br.net.du.fscore.model;
 import java.io.Serializable;
 import java.util.List;
 
-public class Round implements Serializable {
+public class Round implements Serializable, Comparable<Round> {
 
 	private static final long serialVersionUID = 1L;
 
+	private long id;
 	int numberOfCards = 0;
 	List<PlayerRound> data = null;
 
 	public Round() {
 	}
 
+	public int getNumberOfCards() {
+		return numberOfCards;
+	}
+
+	public void setNumberOfCards(int numberOfCards) {
+		this.numberOfCards = numberOfCards;
+	}
+
+	public List<PlayerRound> getData() {
+		return data;
+	}
+
+	public void setData(List<PlayerRound> data) {
+		this.data = data;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public boolean isPersistent() {
+		return this.getId() != 0;
+	}
+
+	@Override
+	public int compareTo(Round other) {
+		if (this.equals(other)) {
+			return 0;
+		}
+
+		// TODO: implement this false return properly
+		return -1;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == null || !(other instanceof Round)) {
+			return false;
+		}
+
+		if (this.numberOfCards == ((Round) other).getNumberOfCards()
+				&& this.data.equals(((Round) other).getData())) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 1;
+		hash = hash * 31 + (data == null ? 0 : data.hashCode());
+		hash = hash * 31 + numberOfCards;
+		return hash;
+	}
+
 	public String toString() {
-		return "this is a Round in a Match";
+		return "Round " + String.valueOf(id);
 	}
 }
