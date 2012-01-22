ffce40a5eb4e63573b4dfe4928c874516f7c9a2f
Eduardo Sztokbant <du@du.net.br>
2012-01-22 21:05:02 -0200

    code some 'equals' tests for Match, Player and Round

--- FScoreTest/src/br/net/du/fscore/test/FScoreInstrumentationTestRunner.java
+++ FScoreTest/src/br/net/du/fscore/test/FScoreInstrumentationTestRunner.java
@@ -1,8 +1,11 @@
-package br.net.du.fscore.test.model;
+package br.net.du.fscore.test;
 
 import junit.framework.TestSuite;
 import android.test.InstrumentationTestRunner;
 import android.test.InstrumentationTestSuite;
+import br.net.du.fscore.test.model.MatchTest;
+import br.net.du.fscore.test.model.PlayerTest;
+import br.net.du.fscore.test.model.RoundTest;
 
 public class FScoreInstrumentationTestRunner extends InstrumentationTestRunner {
 	@Override
@@ -10,6 +13,8 @@ public class FScoreInstrumentationTestRunner extends InstrumentationTestRunner {
 		InstrumentationTestSuite suite = new InstrumentationTestSuite(this);
 
 		suite.addTestSuite(PlayerTest.class);
+		suite.addTestSuite(RoundTest.class);
+		suite.addTestSuite(MatchTest.class);
 
 		return suite;
 	}
