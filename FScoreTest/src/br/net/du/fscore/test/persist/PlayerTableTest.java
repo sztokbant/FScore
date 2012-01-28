870983beeb569408fda51bfba92df653c7d303f1
Eduardo Sztokbant <du@du.net.br>
2012-01-28 11:37:31 -0200

    properly testing PlayerTable.onCreate()

--- FScoreTest/src/br/net/du/fscore/test/persist/PlayerTableTest.java
+++ FScoreTest/src/br/net/du/fscore/test/persist/PlayerTableTest.java
@@ -1,10 +1,12 @@
 package br.net.du.fscore.test.persist;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.test.AndroidTestCase;
 import br.net.du.fscore.persist.DataManagerImpl;
 import br.net.du.fscore.persist.PlayerTable;
+import br.net.du.fscore.persist.PlayerTable.PlayerColumns;
 
 public class PlayerTableTest extends AndroidTestCase {
 
@@ -13,19 +15,20 @@ public class PlayerTableTest extends AndroidTestCase {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-
 		DataManagerImpl dataManager = new DataManagerImpl(getContext());
 		SQLiteOpenHelper openHelper = dataManager.new OpenHelper(getContext(),
 				true);
 		db = openHelper.getWritableDatabase();
-
 		dropTable();
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
 		dropTable();
-		db.close();
+		if (db.isOpen()) {
+			db.close();
+		}
+		super.tearDown();
 	}
 
 	private void dropTable() {
@@ -34,6 +37,14 @@ public class PlayerTableTest extends AndroidTestCase {
 
 	public void testOnCreate() {
 		PlayerTable.onCreate(db);
-		// TODO test something here...
+
+		Cursor cursor = db.query(PlayerTable.NAME, null, null, null, null,
+				null, null);
+
+		// asserts table was created properly and is empty
+		assertEquals(PlayerColumns.get().length, cursor.getColumnCount());
+		assertEquals(0, cursor.getCount());
+
+		cursor.close();
 	}
 }
