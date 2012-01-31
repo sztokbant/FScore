ad9037e8a0a992a900c55d9ab4cc38449b3f3c65
Eduardo Sztokbant <du@du.net.br>
2012-01-31 13:37:17 -0200

    removed unnecessary import

--- FScore/src/br/net/du/fscore/persist/DataConstants.java
+++ FScore/src/br/net/du/fscore/persist/DataConstants.java
@@ -1,7 +1,5 @@
 package br.net.du.fscore.persist;
 
-import android.os.Environment;
-
 public class DataConstants {
 	public static final String DATABASE_NAME = "fscore";
 	public static final String DEBUG_DATABASE_NAME = "fscoredebug";
