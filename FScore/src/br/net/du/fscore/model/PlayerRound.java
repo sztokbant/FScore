370d92b18e75863dc0049ce1be5d5ab93a8100be
Eduardo Sztokbant <du@du.net.br>
2012-02-02 19:39:49 -0200

    PlayerRound now has Round id

--- FScore/src/br/net/du/fscore/model/PlayerRound.java
+++ FScore/src/br/net/du/fscore/model/PlayerRound.java
@@ -2,19 +2,12 @@ package br.net.du.fscore.model;
 
 public class PlayerRound implements Comparable<PlayerRound> {
 
-	private Player player = null;
 	private int bet = 0;
 	private int wins = 0;
+	private Player player = null;
+	private long roundId = 0;
 
 	public PlayerRound(Player player) {
-		this.player = player;
-	}
-
-	public Player getPlayer() {
-		return player;
-	}
-
-	public void setPlayer(Player player) {
 		this.player = player;
 	}
 
@@ -32,6 +25,22 @@ public class PlayerRound implements Comparable<PlayerRound> {
 
 	public void setWins(int wins) {
 		this.wins = wins;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public long getRoundId() {
+		return roundId;
+	}
+
+	public void setRoundId(long roundId) {
+		this.roundId = roundId;
 	}
 
 	@Override
