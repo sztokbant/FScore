58cc0d23c4e131167fd47091c39a0d8afcddfac5
Eduardo Sztokbant <du@du.net.br>
2012-03-27 11:22:52 -0300

    added loc for res/*.xml + more refactoring

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 PROJ_NAME="FScore"
-PROJ_DIR=./FScore/src
-TEST_DIR=./FScoreTest/src
+
+PROJ_DIR="./${PROJ_NAME}/src"
+TEST_DIR="./${PROJ_NAME}Test/src"
+RES_DIR="./${PROJ_NAME}/res"
 
 PROJ_LOC=`find $PROJ_DIR -type f -name *.java -exec cat {} \; | nl | tail -1 | awk '{ print $1 }'`
 TEST_LOC=`find $TEST_DIR -type f -name *.java -exec cat {} \; | nl | tail -1 | awk '{ print $1 }'`
+RES_LOC=`find $RES_DIR -type f -name *.xml -exec cat {} \; | nl | tail -1 | awk '{ print $1 }'`
 
-echo $PROJ_LOC - $PROJ_NAME
-echo $TEST_LOC - ${PROJ_NAME}Test
-echo $(($PROJ_LOC + $TEST_LOC)) - Total
+echo "$PROJ_LOC - $PROJ_NAME"
+echo "$TEST_LOC - ${PROJ_NAME}Test"
+echo "$RES_LOC  - res/"
+echo $(($PROJ_LOC + $TEST_LOC + $RES_LOC)) - Total
